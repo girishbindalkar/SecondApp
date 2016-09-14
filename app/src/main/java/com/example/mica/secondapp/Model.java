904030ac2ab6a8dbf494cb798c9e05f704ff120a
Mica <girishbindalkar@gmail.com>
2016-09-14 10:41:20 +0530

    INta

---
+++ app/src/main/java/com/example/mica/secondapp/Model.java
@@ -0,0 +1,17 @@
+package com.example.mica.secondapp;
+
+/**
+ * Created by Mica on 9/14/2016.
+ */
+public class Model {
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    String name;
+}
